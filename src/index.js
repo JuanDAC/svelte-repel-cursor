b5a9643dc56a2f22ab5c5f632d072bd7c01ff981
JuanDAC <juanavila12.0308@gmail.com>
2021-11-30 08:42:07 -0500

    [🐛] (repare fix of access to method)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-
 const DURATION = 100;
 const TIMING_FUNCTION = 'linear';
 const PROPERTY = 'all';
+const PROPERTY_TIMING = 'transition-timing-function';
 
 /**
  * getWindow:: void -> window object
  */
-const getWindow = new Function('return window')
+const getWindow = new Function('return window');
 
 /**
  * isBrowser:: void -> boolean
@@ -17,90 +17,111 @@ const isBrowser = new Function(`
     } catch {
         return false;
     }
-`)
+`);
 
 /**
- * setTransitionProperties:: (HTMLElement, {duration: number, timingFunction: string, property: string}) -> void
+ * setTransitionProperties:: (
+ *  HTMLElement,
+ *  {duration: number, timingFunction: string, property: string}
+ * ) -> void
  */
-const setTransitionProperties = (node, { duration, timingFunction, property } = {}) => {
-    const { style: { setProperty } } = node;
-    setProperty('transition-duration', `${duration || DURATION}ms`);
-    setProperty('transition-timing-function', timingFunction || TIMING_FUNCTION);
-    setProperty('transition-property', property || PROPERTY);
-}
+const setTransitionProperties = (
+	node,
+	{ duration, timingFunction, property } = {}
+) => {
+	const {
+		style
+	} = node;
+	style.setProperty('transition-duration', `${duration || DURATION}ms`);
+	style.setProperty(PROPERTY_TIMING, timingFunction || TIMING_FUNCTION);
+	style.setProperty('transition-property', property || PROPERTY);
+};
 
 /**
- * controllerEvent: (HTMLElement, number, {active: boolean}) -> MouseEvent -> void
+ * controllerEvent: (HTMLElement, number, {active: boolean}) ->
+ *                  MouseEvent -> void
  */
-const controllerEvent = (node, lengthMotion = DURATION, state) => {
-    const { style: { setProperty } } = node;
-    const { x, y, height, width } = node.getBoundingClientRect();
-    const positionCenterY = y + (height / 2);
-    const positionCenterX = x + (width / 2);
-    const { round } = Math;
-    const { innerHeight, innerWidth } = getWindow();
-    /**
-     * :: MouseEvent -> void
-     */
-    return ({ pageY, pageX }) => {
-        if (state.active) {
-            const repelCursorY = round(((positionCenterY - pageY) / innerHeight) * lengthMotion);
-            const repelCursorX = round(((positionCenterX - pageX) / innerWidth) * lengthMotion);
-            setProperty('transform', `translate(${repelCursorX}px, ${repelCursorY}px)`);
-            state.active = false;
-        }
-    }
-}
+const controllerEvent = (node, motion = DURATION, state) => {
+	const {
+		style
+	} = node;
+	const { x, y, height, width } = node.getBoundingClientRect();
+	const centerY = y + height / 2;
+	const centerX = x + width / 2;
+	const { round } = Math;
+	const { innerHeight: iHeight, innerWidth: iWidth } = getWindow();
+	/**
+	 * :: MouseEvent -> void
+	 */
+	return ({ pageY, pageX }) => {
+		if (state.active) {
+			const repelY = round(((centerY - pageY) / iHeight) * motion);
+			const repelX = round(((centerX - pageX) / iWidth) * motion);
+			style.setProperty('transform', `translate(
+                ${repelX}px,
+                ${repelY}px
+            )`);
+			state.active = false;
+		}
+	};
+};
 
 /**
- * reboundTimmingAnimation:: number -> number
+ * reboundTimmer:: number -> number
  */
-const reboundTimingAnimation = (duration) => Math.ceil(duration * 0.95);
+const reboundTimmer = (duration) => Math.ceil(duration * 0.95);
 
 /**
  * controllerClearOf:: (number, MouseEvent -> void) -> void
  */
 const controllerClearOf = (intervalAnimation, event) => () => {
-    getWindow().removeEventListener('mousemove', event);
-    clearInterval(intervalAnimation);;
-}
-
+	getWindow().removeEventListener('mousemove', event);
+	clearInterval(intervalAnimation);
+};
 
 /**
- * RepelCursor:: (HTMLElement, {duration:number, timingFunction: string, property: string, length: number}) -> {update: property -> RepelCursor, destroy: void -> void}
+ * RepelCursor:: (
+ *  HTMLElement,
+ *  {duration:number, timingFunction: string, property: string, length: number}
+ * ) -> {update: property -> RepelCursor, destroy: void -> void}
  */
 const RepelCursor = (node, data) => {
-    if (!isBrowser()) {
-        return {};
-    }
+	if (!isBrowser()) {
+		return {};
+	}
 
-    if (!data) {
-        return {
-            update: (property) => RepelCursor(node, property),
-        }
-    }
+	if (!data) {
+		return {
+			update: (property) => RepelCursor(node, property)
+		};
+	}
 
-    const state = { active: false };
+	const state = {
+		active: false
+	};
 
-    setTransitionProperties(node, data);
+	setTransitionProperties(node, data);
 
-    const { duration, length } = data;
+	const { duration, length } = data;
 
-    const intervalAnimation = setInterval(() => repelActive = true, reboundTimingAnimation(duration))
+	const intervalAnimation = setInterval(
+		() => (state.active = true),
+		reboundTimmer(duration)
+	);
 
-    const event = controllerEvent(node, length, state);
+	const event = controllerEvent(node, length, state);
 
-    getWindow().addEventListener('mousemove', event);
+	getWindow().addEventListener('mousemove', event);
 
-    const toClear = controllerClearOf(intervalAnimation, event);
+	const toClear = controllerClearOf(intervalAnimation, event);
 
-    return {
-        update(property) {
-            toClear();
-            return RepelCursor(node, property);
-        },
-        destroy: () => toClear(),
-    };
+	return {
+		update(property) {
+			toClear();
+			return RepelCursor(node, property);
+		},
+		destroy: () => toClear()
+	};
 };
 
 export default RepelCursor;
